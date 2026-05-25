cfe071e8aa3ba450a202974c74a60966a22e7a3f
Lea Sony Jacob <lea.jacob@g10x.com>
2026-05-25 13:10:50 +0530

    flake8 issue resolved

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def home():
     return "Pipeline Learning Project Running Successfully"
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
--- test_app.py
+++ test_app.py
@@ -1,7 +1,7 @@
 from app import app
 
-client = app.test_client()
 
 def test_home():
-    response = client.get('/')
+    client = app.test_client()
+    response = client.get("/")
     assert response.status_code == 200
